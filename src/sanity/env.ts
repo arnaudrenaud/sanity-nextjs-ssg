dbf95743135b8082501c968dae0ef876696b5fb6
Arnaud Renaud <arnaud.renaud@gmail.com>
2025-05-29 19:06:40 +0200

    Make environment variables available browser-side
    This will enable client-side data fetching (e.g. infinite scroll).

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -2,18 +2,18 @@ export const apiVersion =
   process.env.SANITY_STUDIO_SANITY_API_VERSION || "2025-04-14";
 
 export const dataset = assertValue(
-  process.env.SANITY_STUDIO_SANITY_DATASET,
-  "Missing environment variable: SANITY_STUDIO_SANITY_DATASET"
+  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  "Missing environment variable: NEXT_PUBLIC_SANITY_DATASET",
 );
 
 export const hostId = assertValue(
-  process.env.SANITY_STUDIO_HOST,
-  "Missing environment variable: SANITY_STUDIO_HOST"
+  process.env.NEXT_PUBLIC_SANITY_HOST,
+  "Missing environment variable: NEXT_PUBLIC_SANITY_HOST",
 );
 
 export const projectId = assertValue(
-  process.env.SANITY_STUDIO_SANITY_PROJECT_ID,
-  "Missing environment variable: SANITY_STUDIO_SANITY_PROJECT_ID"
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID",
 );
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
